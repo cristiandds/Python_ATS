874ba65140369eb708f65a99a13db35515d16ced
cr7 <jmcristian2@gmail.com>
2025-04-10 11:58:33 -0500

    Carpeta elementos basicos

---
+++ Elementos_basicos/3_operadores_aritmeticos/operadores.py
@@ -0,0 +1,15 @@
+# operadores aritméticos 
+# +-*/
+# modulo %
+# potenciación **
+
+'''
+num1= 3
+num2 = 5
+resultado = num1 ** num2
+print(resultado)
+'''
+
+resultado= 3**3 * (13/5 - (2*4))
+print(resultado)
+
