3e0322c3ddd4ff949e4c0df9ce0c262a97913aac
cr7 <jmcristian2@gmail.com>
2025-04-11 22:08:33 -0500

    entrada de datos: textos, #enteros y #decimales

--- Elementos_basicos/4_manejo_de_informacion/entrada.py
+++ Elementos_basicos/4_manejo_de_informacion/entrada.py
@@ -1,1 +1,13 @@
+# entrada de datos
 
+# Para textos
+nombre = input("Digite su nombre: ")
+print(f"Hola {nombre}")
+
+# Para numeros
+numero = int(input("Digite un numero: "))
+print(f"El numero es {numero+5}")
+
+# Para numeros con decimales
+numero = float(input("Digite otro numero: "))
+print(f"El numero es {numero}")
