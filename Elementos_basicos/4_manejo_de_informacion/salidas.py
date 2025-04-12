98b4093e87297a8ed324f0b7f1795f7f5ba9bc47
cr7 <jmcristian2@gmail.com>
2025-04-11 21:47:32 -0500

    salida de informacion

---
+++ Elementos_basicos/4_manejo_de_informacion/salidas.py
@@ -0,0 +1,16 @@
+# salidas 
+
+# forma 1
+'''nombre = "Cristian"
+edad = 22
+print("Hola",nombre,"tienes",edad,"años")'''
+
+# forma 2
+'''nombre = "Cristian"
+edad = 22
+print("Hola {} tienes {} años".format(nombre,edad))'''
+
+# forma 3 
+nombre = "Cristian"
+edad = 22
+print(f"Hola {nombre} tienes {edad} años")
