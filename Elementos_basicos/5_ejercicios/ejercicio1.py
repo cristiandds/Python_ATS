8a818033555ea67d753dfe8bc0e09ef742532fe9
cr7 <jmcristian2@gmail.com>
2025-04-17 16:24:11 -0500

    ejercicios

---
+++ Elementos_basicos/5_ejercicios/ejercicio1.py
@@ -0,0 +1,10 @@
+#ejercicio1
+'''
+    Escribir la siguiente expresión en forma de expresión algorítmica
+    a**3 * (b**2 - 2*a*c) / 2*b
+'''
+a=float(input("a--> "))
+b=float(input("b--> "))
+c=float(input("c--> "))
+resultado= (a**3 * (b**2 - 2*a*c)) / (2*b)
+print(f"El resultado es : {resultado}")
