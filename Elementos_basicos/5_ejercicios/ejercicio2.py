246875e2e137c2ec0b83fecdc4bad1f5294669ab
cr7 <jmcristian2@gmail.com>
2025-04-17 16:37:56 -0500

    ejercicio #2

---
+++ Elementos_basicos/5_ejercicios/ejercicio2.py
@@ -0,0 +1,9 @@
+#ejercico 2
+'''
+    Determinar la solución lógica de la siguiente operación:
+    ((3+5x8)<3 and ((-6/3x4)+2<2)) or (a>b)
+'''
+a=float(input("a--> "))
+b=float(input("b--> "))
+resultado=((3+5*8)<3 and (-6/3*4)+2<2) or (a>b)
+print(f"El resultado es: {resultado}")
