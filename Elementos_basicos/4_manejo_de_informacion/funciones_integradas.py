4beb7606e155fc566ba7a3a0d07f114548ec9700
cr7 <jmcristian2@gmail.com>
2025-04-17 16:03:10 -0500

    funciones integradas

---
+++ Elementos_basicos/4_manejo_de_informacion/funciones_integradas.py
@@ -0,0 +1,23 @@
+# Funciones integradas
+n=str(10)
+print(n)
+#convertir un numero a binario
+n=bin(10)
+print(n)
+#convertir un numero a hexadecimal
+n=hex(10)
+print(n)
+#binario a entero
+n=int("0b1010",2)
+print(n)
+#hexadecimal a un entero
+n=int("0xa",16)
+#valor absoluto de un numero
+n=abs(-8)
+print(n)
+#funcion round: redondea
+n=round(5.6)
+print(n)
+#funcion len: cuanta los caracteres
+n=len("Cristian")
+print(n)
