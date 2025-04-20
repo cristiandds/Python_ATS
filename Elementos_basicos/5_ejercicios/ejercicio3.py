04540cf6aebc25f1465b135a0b97825d743ddaf7
cr7 <jmcristian2@gmail.com>
2025-04-20 05:31:36 -0500

    ejercicio numero 3

---
+++ Elementos_basicos/5_ejercicios/ejercicio3.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 3:
+Hacer un programa que pida un carácter e indique si es una vocal o no
+'''
+
+letra=input("Escriba un caracter: ").lower() #upper() transformar a mayuscula
+if letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u':
+    print("Es una vocal")
+else:
+    print("No es una vocal")
